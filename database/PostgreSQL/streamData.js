13412a7180ac2b589dbd6a73639705abf8a8268c
KweonMo Koo <mikekoo2010@gmail.com>
2019-03-04 22:11:30 -0800

    Create function that writes both similar and like products

--- database/PostgreSQL/streamData.js
+++ database/PostgreSQL/streamData.js
@@ -4,26 +4,19 @@ const {
   randomNumberDec,
   randomNumberInt,
   randomNumberArr,
-  generateSimilarList,
-  generateLikeList
+  getTrueByChance,
 } = require('../helpers.js');
-    
+
+// Create write streams for three tables
 const products = fs.createWriteStream('products.csv');
-const similarProducts = fs.createWriteStream('similarProducts.csv')
+const similarProducts = fs.createWriteStream('similarProducts.csv');
+const likeProducts = fs.createWriteStream('likeProducts.csv');
 
-const getRand = () => {
-  return Math.floor(Math.random() * 10000000);
-}
-
-const getTrueByChance = (chance) => {
-  return Math.random >= chance;
-}
-
+// Generate data string for CSV format
 const dataGeneration = (i) => {
-  // let result = [];
-  let str = '';
-
-  let id = i, 
+  // Data Initialization
+  let str = '', 
+    id = i, 
     product_name = faker.random.words(),
     category = faker.commerce.productAdjective(),
     size = randomNumberArr(randomNumberInt(1,5)),
@@ -40,6 +33,7 @@ const dataGeneration = (i) => {
     price = randomNumberDec(1,200),
     image = Math.floor(Math.random() * 1000) + 1;
 
+  // Create string
   str = `
     ${id},
     ${product_name},
@@ -61,7 +55,8 @@ const dataGeneration = (i) => {
   return str;
 }
 
-const similarProductsDataGeneration = (i) => {
+// Create similar or like products list
+const similarOrLikeProductsDataGeneration = (i) => {
   let similarStr = `${i},`;
   for (let j = 0; j < 15; j++) {
     (
@@ -70,11 +65,11 @@ const similarProductsDataGeneration = (i) => {
       : similarStr += getRand() + ','
     )
   }
-
-  return similarStr
+  return similarStr;
 }
 
-const writeProductsOneMillionTimes = (productsFile) => {
+// Generate ten million records
+const writeProductsTenMillionTimes = (productsFile) => {
   let upperLim = 10000000;
   var i = 0;
   write();
@@ -93,11 +88,10 @@ const writeProductsOneMillionTimes = (productsFile) => {
         online_only, limited_edition, free_shipping, price, image\n
         `, 'utf-8');
       } else {
-        // storage.push(product.similar)
         // see if we should continue, or wait
         // don't pass the callback, because we're not done yet.
         ok = productsFile.write(dataGeneration(i), 'utf-8');
-          if (i % 100000 === 0) console.log('Writing one million products:',i)
+          if (i % 100000 === 0) console.log('Writing ten million products:',i)
       }
     } while (i < upperLim && ok);
       if (i > 0) {
@@ -109,7 +103,7 @@ const writeProductsOneMillionTimes = (productsFile) => {
   }
 }
 
-const writeSimilarOneMillionTimes = (similarProducts) => {
+const writeSimilarOrLikeTenMillionTimes = (similarOrLikeProducts) => {
   let upperLim = 10000000;
   var i = 0;
   write();
@@ -119,26 +113,26 @@ const writeSimilarOneMillionTimes = (similarProducts) => {
       i += 1;
       if (i === upperLim) {
         // last iteration
-        similarProducts.write(similarProductsDataGeneration(i), 'utf-8');
+        similarOrLikeProducts.write(similarOrLikeProductsDataGeneration(i), 'utf-8');
         console.log('DONE');
       } else if (i === 1) {
-        similarProducts.write(`#id,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15\n`)
+        similarOrLikeProducts.write(`#id,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15\n`)
       } else {
         // see if we should continue, or wait
         // don't pass the callback, because we're not done yet.
-        ok = similarProducts.write(similarProductsDataGeneration(i), 'utf-8');
-        if (i % 100000 === 0) console.log("Generating one million similar products list:",i)
+        ok = similarOrLikeProducts.write(similarOrLikeProductsDataGeneration(i), 'utf-8');
+        if (i % 100000 === 0) console.log("Generating ten million similar products list:",i);
       }
     } while (i < upperLim && ok);
       if (i > 0) {
         // had to stop early!
         // write some more once it drains
-        similarProducts.once('drain', write);
+        similarOrLikeProducts.once('drain', write);
     }
     return;
   }
-
 }
 
-writeProductsOneMillionTimes(products);
-writeSimilarOneMillionTimes(similarProducts);
+writeProductsTenMillionTimes(products);
+writeSimilarOrLikeTenMillionTimes(similarProducts);
+writeSimilarOrLikeTenMillionTimes(likeProducts);
