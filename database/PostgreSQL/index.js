c0bdfef3c5170d1c4a543ed19290e23cfbdccefd
KweonMo Koo <mikekoo2010@gmail.com>
2019-03-07 15:18:59 -0800

    Separate testing function to a separate file

--- database/PostgreSQL/index.js
+++ database/PostgreSQL/index.js
@@ -1,7 +1,6 @@
 const { Pool, Client } = require('pg');
-const pool = new Pool();
 
-await pool.connect({
+const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'similar_and_like'
