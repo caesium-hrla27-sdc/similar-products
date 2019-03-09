590040cf58a7f1c85ead5a106e807c1e2760a096
KweonMo Koo <mikekoo2010@gmail.com>
2019-03-09 12:35:06 -0800

    Fix bug to invoke random function

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import SimilarApp from './components/SimilarApp';
 import LikeApp from './components/LikeApp';
 
-let id = Math.floor(Math.random * 10000000) + 1;
+let id = Math.floor(Math.random() * 10000000) + 1;
 
 render(<SimilarApp id={id}/>, document.getElementById('similar'));
 render(<LikeApp id={id}/>, document.getElementById('youMayLike'));
