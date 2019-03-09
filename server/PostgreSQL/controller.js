b9027eb8d8a2b1fcb6556e72b09de811f2d90e19
KweonMo Koo <mikekoo2010@gmail.com>
2019-03-08 18:50:05 -0800

    Route to Postgres methods

--- server/PostgreSQL/controller.js
+++ server/PostgreSQL/controller.js
@@ -4,23 +4,19 @@ const getSimilar = (req, res) => {
   console.time('QUERY SIMILAR PRODUCTS')
   let { id } = req.params;
   
-  pool.query(`SELECT * FROM similars WHERE id=${id};`, (err, data) => {
+  pool.query(`SELECT kind FROM products WHERE id=${id};`, (err, data) => {
     if (err) {
       console.error(err);
     } else {
-      let obj = data.rows[0];
-      let response = [];
-      for (let key in obj) {
-        await pool.query(`SELECT * FROM products WHERE id=${obj[key]};`, (err, product) => {
-          if (err) {
-            console.error(err);
-          } else {
-            response.push(product.rows[0])
-          }
-        })
-      }
-      console.timeEnd('QUERY SIMILAR PRODUCTS');        
-      res.status(200).json(response);
+      let similarId = data.rows[0].category;
+      pool.query(`SELECT * FROM products p INNER JOIN kinds k ON k.id=p.kind WHERE k.id=${similarId} LIMIT 15;`, (err, product) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.timeEnd('QUERY SIMILAR PRODUCTS');        
+          res.status(200).json(response);
+        }
+      })
     }
   })
 }
