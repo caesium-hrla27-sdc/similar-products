e4b54d5cc514912cd41556c649ebc3936fccd0ae
KweonMo Koo <mikekoo2010@gmail.com>
2019-03-08 18:49:12 -0800

    Test two queries vs single query

--- database/PostgreSQL/test.js
+++ database/PostgreSQL/test.js
@@ -5,32 +5,24 @@ const pool = new Pool({
   host: 'localhost',
   database: 'similar_and_like'
 });
-
-
-
-for (let i = 3999400; i < 3999500; i++) {
-  let counter = 0;
+// SELECT * FROM products p1 INNER JOIN products p2 ON p1.kind=p2.kind WHERE p1.id=${i} LIMIT 15
+// SELECT kind FROM products WHERE id=${i}
+for (let i = 444800; i < 444900; i++) {
   console.time(`QUERY SIMILAR PRODUCTS id: ${i}`)
-  pool.query(`SELECT * FROM similars WHERE id=${i};`, function(err, data) {
+  pool.query(`SELECT * FROM products p1 INNER JOIN products p2 ON p1.kind=p2.kind WHERE p1.id=${i} LIMIT 15;`, function(err, data) {
     if (err) {
       console.error(err);
-    } else {
-      let obj = data.rows[0];
-      let response = [];
-      for (let key in obj) {
-        pool.query(`SELECT * FROM products WHERE id=${obj[key]};`, function(err, product) {
-          counter += 1;
-          if (err) {
-            console.error(err);
-          } else {
-            response.push(product.rows[0]);
-          }
-          if (counter === 16) {
-            console.timeEnd(`QUERY SIMILAR PRODUCTS id: ${i}`);  
-          }
-        })
-      }
-    }
+    // } else {
+    //   let similarId = Number(data.rows[0].kind);
+    //   pool.query(`SELECT * FROM products p INNER JOIN kinds k ON k.id=p.kind WHERE k.id=${similarId} LIMIT 15;`, function(err, product) {
+    //     if (err) {
+    //       console.error(err);
+        } else {
+          // console.log(product)
+          console.timeEnd(`QUERY SIMILAR PRODUCTS id: ${i}`);  
+        }
+    //   })
+    // }
   })
 }
 
