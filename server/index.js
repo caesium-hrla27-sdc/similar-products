6c5d9d79ad410ea5b105713c05ef2bd9b583763b
KweonMo Koo <mikekoo2010@gmail.com>
2019-03-09 12:35:56 -0800

    Add routings for full CRUD functionality

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const parser = require('body-parser');
 const morgan = require('morgan');
 const PORT = 3004;
 const db = require('../database/PostgreSQL/index.js');
-const { getSimilar, getLike } = require('./PostgreSQL/controller.js');
+const { getSimilar, getLike, updateLove, putProduct, deleteProduct } = require('./PostgreSQL/controller.js');
 
 const app = express();
 
@@ -16,5 +16,8 @@ app.use(express.static(path.resolve(__dirname, '../public')));
 
 app.get('/similar/:id', getSimilar);
 app.get('/like/:id', getLike);
+app.put('/love/:id', updateLove);
+app.post('/basket/in/:id', putProduct);
+app.delete('/basket/out/:id', deleteProduct);
 
 app.listen(PORT, () => console.log('Listening to port ', PORT));
