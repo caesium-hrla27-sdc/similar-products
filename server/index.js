c968aa6259451c8b5b1ba3401ef4a05cfa3c8b51
KweonMo Koo <mikekoo2010@gmail.com>
2019-03-12 14:26:00 -0700

    Take off morgan

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
 const parser = require('body-parser');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const PORT = 3004;
 const db = require('../database/PostgreSQL/index.js');
-const { getSimilar, getLike, updateLove, putProduct, deleteProduct } = require('./PostgreSQL/controller.js');
+const { getSimilar, getLike, updateLove, postProduct, deleteProduct } = require('./PostgreSQL/controller.js');
 
 const app = express();
 
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
@@ -17,7 +17,7 @@ app.use(express.static(path.resolve(__dirname, '../public')));
 app.get('/similar/:id', getSimilar);
 app.get('/like/:id', getLike);
 app.put('/love/:id', updateLove);
-app.post('/basket/in/:id', putProduct);
+app.post('/basket/in/:id', postProduct);
 app.delete('/basket/out/:id', deleteProduct);
 
 app.listen(PORT, () => console.log('Listening to port ', PORT));
