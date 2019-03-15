43e8cbac766e380e9fdb16ac46de438f753621d1
KweonMo Koo <mikekoo2010@gmail.com>
2019-03-14 18:46:30 -0700

    Switch render to hydrate

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { hydrate } from 'react-dom';
 import SimilarApp from './components/SimilarApp';
 import LikeApp from './components/LikeApp';
 
 let id = Math.floor(Math.random() * 10000000) + 1;
 
-render(<SimilarApp id={id}/>, document.getElementById('similar'));
-render(<LikeApp id={id}/>, document.getElementById('youMayLike'));
+hydrate(<SimilarApp id={id}/>, document.getElementById('similar'));
+hydrate(<LikeApp id={id}/>, document.getElementById('youMayLike'));
